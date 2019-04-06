d5cfbcdf033be9564249b92cf3d988f420b0da24
zoyazee <joygathigira@gmail.com>
2019-04-06 09:25:24 +0300

    python assignment

---
+++ calculator.py
@@ -0,0 +1,31 @@
+def add(a,b):
+	c=a+b
+	print(c)
+
+def subtract(c,d):
+	d=c-d
+	print(d)
+
+def multiply(e,f):
+	f=e*f
+	print(f)
+
+def division(g,h):
+	z=g/h
+	print(z)
+
+def modulus(j,k):
+	y=j%k
+	print(y)
+
+def exponent(m,n):
+	f=m**n
+	print(f)
+
+def whole_number_division(g,h):
+	a=g//h
+	print(a)
+
+def percentage(h,k):
+	s=(h/k)*100
+	print(s)
